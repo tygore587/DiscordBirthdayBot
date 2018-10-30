6ebe7acf4cdde76f6a7442e2043c1ccae28d5805
Lycran <alullric@uni-potsdam.de>
2018-10-30 13:32:45 +0100

    path + first test command
    + added path to json
    + added first test command
      - message send not working

--- Bot.py
+++ Bot.py
@@ -8,19 +8,25 @@ Created on Thu Oct 18 17:13:31 2018
 
 import discord
 import json
+import os
 
-token = json.loads(open('token.json').read())['token']
+cwd = os.getcwd()
+
+token = json.loads(open(cwd + '/Documents/GitHub/DiscordBirthdayBot/token.json').read())['token']
 
 
 client = discord.Client()
 
 @client.event
-
 async def on_ready():
     print(f"We have logged in as {client.user}")
 
 @client.event
 async def on_message(message):
     print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
+    if message.content == '?test':
+        print(f"success")
+        await message.channel.send("success")
+
 
 client.run(token)
