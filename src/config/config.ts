1af3246a68d7c014d22d094c1ce12916999ae483
Martin <martin@mstaehr.net>
2020-02-23 02:22:20 +0100

    change config to be dependend on node_env instead of of custom env

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import '../env';
 export class Config {
     static OWNER: string | undefined = process.env.OWNER;
     static TOKEN: string | undefined = process.env.TOKEN;
-    static IS_DEVELOPMENT: boolean = process.env.ENVIRONMENT === 'development';
+    static IS_DEVELOPMENT: boolean = process.env.NODE_ENV === 'development';
     static PREFIX: string = Config.IS_DEVELOPMENT ? '?' : '!';
     static STATUS_MESSAGE: string = Config.IS_DEVELOPMENT ? 'Ich werde gerade entwickelt.' : 'Ich bin produktiv.';
     static DATABASE_URL: string | undefined = process.env.DATABASE_URL;
